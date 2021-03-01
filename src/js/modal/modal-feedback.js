bb971787d254dfca704e81c8b7faef9f757fbae6
Kirill Foshchenko <k.foshchenko@yandex.ru>
2021-03-01 22:48:00 +0300

    update modal-feedback

--- src/js/modal/modal-feedback.js
+++ src/js/modal/modal-feedback.js
@@ -1,6 +1,7 @@
-let btn = document.querySelector('.page-button--request')
-let services = document.querySelector('.services')
-let btnServ = services.querySelector('.page-button--request')
+let btn = document.querySelector('.page-button--chat')
+let ph = document.querySelector('.page-header')
+let phBtn = ph.querySelector('.page-button--chat')
+let menu = document.querySelector('.menu')
 let fb = document.querySelector('.feedback')
 let fbClose = fb.querySelector('.page-button--close')
 let opacity = document.querySelector('.opacity')
@@ -13,7 +14,18 @@ export function modalFeedback () {
       opacity.classList.add('opacity--open')
     }
     fb.classList.add('feedback--open')
+    menu.classList.remove('menu--open')
   })
+
+  if (window.innerWidth >= 768 && window.innerWidth < 1366) {
+    phBtn.addEventListener('click', evt => {
+      evt.preventDefault()
+
+      opacity.classList.add('opacity--open')
+      fb.classList.add('feedback--open')
+      menu.classList.remove('menu--open')
+    })
+  }
   
   fbClose.addEventListener('click', evt => {
     evt.preventDefault()
@@ -22,13 +34,6 @@ export function modalFeedback () {
       opacity.classList.remove('opacity--open')
     }
     fb.classList.remove('feedback--open')
-  })
-  
-  btnServ.addEventListener('click', evt => {
-    evt.preventDefault()
-
-    opacity.classList.add('opacity--open')
-    fb.classList.add('feedback--open')
   })
 
   opacity.addEventListener('click', evt => {
